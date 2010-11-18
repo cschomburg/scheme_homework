0fdbdbe21c69a943ad9791d8e9bbcbcb2b272dfb
xconstruct <xconstruct@gmail.com>
2010-11-18 21:24:22 +0100

    uebung 03: der letzte schliff vorm losschicken

--- 03-schomburg_constantin.rkt
+++ 03-schomburg_constantin.rkt
@@ -21,7 +21,7 @@
 ;;### Aufgabe 1 ###
 ;;#################
 
-;; Alle Zahlen bis n aufsummieren, die durch 3 oder teilbar sind
+;; Alle Zahlen bis n aufsummieren, die durch 3 oder 5 teilbar sind
 (define (aufgabe1 n)
   (define (iter n sum)
     (cond ((<= n 0) sum)
@@ -48,7 +48,7 @@
               (+ counter 1))))
   (iter 1 1))
 
-;; Compute-e mit Verwendung von factorial (rekursiv)
+;; Compute-e mit Verwendung von factorial
 (define (compute-e1 n)
   (define (iter m sum)
     (if (> m n) sum
@@ -56,7 +56,7 @@
               (+ sum (/ 1 (factorial m))))))
   (iter 0 0.))
 
-;; Compute-e ohne Verwendung von factorial (iterativ)
+;; Compute-e ohne Verwendung von factorial
 (define (compute-e2 n)
   (define (iter m value term)
     (if (> m n) (+ value term)
@@ -95,13 +95,13 @@
 
 ;; Der rekursive Prozess hat eine exponentielle Laufzeit von O(1.84^n), da
 ;; er beim Berechnen immer auf die Werte von rekursiven Prozeduren zurückgreifen muss (und oftmals doppelt bis dreifach berechnet).
-;; Im Gegensatz läuft der iterative Prozess schneller, da er auf vorher berechnete
+;; Im Gegensatz läuft der iterative Prozess deutlich schneller, da er auf vorher berechnete
 ;; Eregebnisse zurückgreift. Ich schätze, er läuft mit O(n), die realen Werte deuten
 ;; aber eher auf O(n^2) hin - vielleicht wegen der großen Zahlen?
 
 ;; Proportionalitätsfaktoren an meinem Desktop-Computer:
 ;; rekursiv: ~ 9e-05 +- 3e-05
-;; iterativ: ~ 0.04  +- 0.03
+;; iterativ: ~ 6e-07 +- 2e-07
 
 ;; O-Funktionen
 (define (f-rec-o n) (expt 1.84 n))
@@ -126,12 +126,16 @@
           (else (fast-exp-iter a (* b b) (/ n 2)))))
   (fast-exp-iter 1 b n))
 
-(cputime fast-exp-i 2 1e9)
-
 ;; O-Funktion
 (define (fast-exp-i-o n) (expt 3 (log n)))
 
-;(/ (fast-exp-i-o 5e15) 1e3 60 60 24 365 1e6) ;; 5.6 Millionen Jahre
+;; Proportionalitätsfaktor an meinem Desktop-Rechner:
+;; 2e-07 +- 1e-07
+
+;(factor fast-exp-i-o 1e8 fast-exp-i 2 1e8)
+;(factor fast-exp-i-o 1e9 fast-exp-i 2 1e9)
+
+;(/ (* 2e-07 (fast-exp-i-o 5e15)) 1e3 60 60 24 365) ;; 1.12 Jahre
 
 
 
@@ -158,5 +162,5 @@
 
 (start-test "fast-exp-i")
 (test "1^0" (fast-exp-i 1 0) 1)
-(test "3^2" (fast-exp-i 3 2) 9)
+(test "3^3" (fast-exp-i 3 3) 27)
 (test "2^10" (fast-exp-i 2 10) 1024)
