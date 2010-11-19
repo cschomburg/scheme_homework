639cd0d2e8b924f9a00220a22803aee891636d60
xconstruct <xconstruct@gmail.com>
2010-11-19 16:45:27 +0100

    uebung 04: fertig!

--- 04-schomburg_constantin.rkt
+++ 04-schomburg_constantin.rkt
@@ -8,11 +8,11 @@
 ;;### Aufgabe 1 ###
 ;;#################
 
-;;### Aufgabenteil a) ###
+;;### Aufgabenteil 1a) ###
 ;; Let öffnet einen neuen Block, in dem pi als lokale Variable neu definiert wird.
 ;; Da sich (* pi d) in diesem Block befindet, wird das lokale Pi verwendet und nicht im globalen Environment nachgeschaut.
 
-;;### Aufgabenteil b) ###
+;;### Aufgabenteil 1b) ###
 ;; Die lokalen Variablen im let-Aufruf werden simultan definiert, also werden bei der Auswertung die Werte aus den Variablen höheren Gültigkeitsbereiches genommen statt der neu definierten. Beispiel pi: die anderen Variablen-Definitionen verwenden den Wert aus dem Global Environment statt aus let.
 
 ;; Oberfläche eines Zylinders
@@ -27,22 +27,22 @@
 ;(cylinder-area 2 3) ;; 25.13272
 
 
-;;### Aufgabenteil c) ###
+;;### Aufgabenteil 1c) ###
 
 ;; Umfang eines Kreises, Lambda-Schreibweise
-(define (circle-circumference-lambda d)
+(define circle-circumference-lambda (lambda (d)
   ((lambda (pi)
-    (* pi d)) 3.14159))
+    (* pi d)) 3.14159)))
 
 ;; Oberfläche eines Zylinders, Lambda-Schreibweise
-(define (cylinder-area-lambda d h)
+(define cylinder-area-lambda (lambda (d h)
   ((lambda (pi r)
      ((lambda (k s)
         ((lambda (b)
            (+ b s))
          (* 2 k)))
       (* pi r r) (* 2 pi r h)))
-   3.14159 (/ d 2)))
+   3.14159 (/ d 2))))
 
 
 
@@ -64,7 +64,8 @@
           (let ((e (remainder (+ (* 2 b) (* 4 c) (* 6 d) N) 7)))
             (let ((o (+ 22 d e)))
               (cond ((> o 31) (display (- o 31)) (display ". April"))
-                    (else (display o) (display ". März") (newline))))))))))
+                    (else (display o) (display ". März")))
+              (newline))))))))
 
 ;; let-Ausdrücke: 6
 
@@ -78,20 +79,21 @@
 (define (compose f1 f2 x)
   (f1 (f2 x)))
 
-(compose (lambda (x) (expt x 2))
-         (lambda (x) (- (* 2 x) 3))
-         6) ;; 81
+;(compose (lambda (x) (expt x 2))
+;         (lambda (x) (- (* 2 x) 3))
+;         6) ;; 81
+
+
+;;### Aufgabenteil 3b) ###
 
 (define (h^k h k)
   (cond ((= k 1) (lambda (x) (h x)))
         ((> k 1) (lambda (x) (h ((h^k h (- k 1)) x))))))
 
-;;### Aufgabenteil b) ###
-
 (define (square x) (* x x))
 (define f (h^k square 4))
 
-(f 2) ;; 65536
+;(f 2) ;; 65536
 
 
 
@@ -114,11 +116,27 @@
   (inner-iter start-value start-index))
 
 
-;;### Aufgabenteil b) ####
+;;### Aufgabenteil 4b) ####
 
 (define (phi n)
-  (iterate 1
-           (
+  (iterate 1.
+           (lambda (cur-val cur-index) (+ 1 (/ 1 cur-val)))
+           0
+           (lambda (cur-val cur-index) (+ cur-index 1))
+           (lambda (cur-val cur-index) (>= cur-index n))))
+
+
+;;### Aufgabenteil 4c) ####
+
+(define (h^k-iterate h k)
+  (lambda (x)
+    (iterate x
+             (lambda (cur-val cur-index) (h cur-val))
+             k
+             (lambda (cur-val cur-index) (- cur-index 1))
+             (lambda (cur-val cur-index) (< cur-index 1)))))
+ 
+
 
 ;;##################
 ;;### Testreihen ###
@@ -127,10 +145,17 @@
 (require "tests.rkt")
 
 (start-test "Aufgabe 1")
-(test "Umfang mit Lambda von 2: " (circle-circumference-lambda 2) 6.28318)
-(test "Zylinderfläche Lambda/let" (cylinder-area-lambda 2 3) (cylinder-area 2 3))
+(test "Umfang lambda: " (circle-circumference-lambda 2) 6.28318)
+(test "Zylinderfläche lambda/let" (cylinder-area-lambda 2 3) (cylinder-area 2 3))
 ;(start-test "Osterdatum")
 ;(test "2010" (osterdatum 2010) "4. April")
 ;(test "2011" (osterdatum 2011) "24. April")
 (start-test "Aufgabe 3")
 (test "Komposition" (compose (lambda (x) (expt x 2)) (lambda (x) (- (* 2 x) 3)) 6) 81)
+(test "h^k: f(2)" (f 2) 65536)
+(start-test "Iteration")
+(test "Phi(0)"  (phi  0) 1.0)
+(test "Phi(1)"  (phi  1) 2.0)
+(test "Phi(2)"  (phi  2) 1.5)
+(test "Phi(10)" (phi 10) 1.6179775280898876)
+(test "h^k-iterate" ((h^k-iterate square 4) 2) 65536)
